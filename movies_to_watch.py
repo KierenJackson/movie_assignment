86430ac99b8fa712aef1087cc427da39de79ddc0
KierenJackson <kieren.jacksonclapper@my.jcu.edu.au>
2019-08-25 23:01:23 +1000

    Initial commit

---
+++ movies_to_watch.py
@@ -0,0 +1,14 @@
+"""
+Name: Kieren Jackson
+Date started: 25/08/19
+GitHub URL:
+"""
+
+
+def main():
+    print("Movies To Watch 1.0 - by Kieren Jackson")
+
+
+if __name__ == '__main__':
+    main()
+
