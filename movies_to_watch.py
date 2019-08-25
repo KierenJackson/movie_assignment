77d044d8f190b773dea279605832176c6f0e680c
KierenJackson <kieren.jacksonclapper@my.jcu.edu.au>
2019-08-26 01:46:38 +1000

    Split movie list function into another function called movie sorter

--- movies_to_watch.py
+++ movies_to_watch.py
@@ -9,18 +9,30 @@ MOVIES_FILE = "movies.csv"
 
 
 def main():
-
+    movies = movie_sorter()
 
     print("Movies To Watch 1.0 - by Kieren Jackson")
     print_menu()
-    menu_answer = menu_selection()
+    menu_answer = menu_input()
+    menu_selection(menu_answer, movies)
+
+
+def movie_sorter():
+    movie_file = open(MOVIES_FILE, "r")
+    movies = movie_file.readlines()
+    movie_file.close()
+
+    for n in range(len(movies)):
+        movies[n] = movies[n].split(",")
+
+    return movies
 
 
 def print_menu():
     print("Menu: \nL - List movies \nA - Add new movie \nW - Watch a movie \nQ - Quit")
 
 
-def menu_selection():
+def menu_input():
     answer = input(">>> ").upper()
     while answer not in "LAWQ":
         print("Invalid menu choice")
@@ -29,20 +41,30 @@ def menu_selection():
     return answer
 
 
-def movie_list():
-    number = 1
-    movie_file = open(MOVIES_FILE, "r")
-    movies = movie_file.readlines()
-    movie_file.close()
+def menu_selection(answer, movies):
+    if answer == "L":
+        movie_list(movies)
+        print_menu()
+        menu_answer = menu_input()
+        menu_selection(menu_answer, movies)
+    elif answer == "A":
+        pass
+    elif answer == "W":
+        pass
+    elif answer == "Q":
+        print("{} movies saved to movies.csv".format(len(movies)))
+        print("Have a nice day :)")
+        quit
 
-    for n in range(len(movies)):
-        movies[n] = movies[n].split(",")
+
+def movie_list(movies):
+    number = 1
 
     for movie in movies:
         print("{}. {:35} - {:>5} ({})".format(number, movie[0], movie[1], movie[2]))
         number += 1
 
-6
+
 if __name__ == '__main__':
     main()
 
