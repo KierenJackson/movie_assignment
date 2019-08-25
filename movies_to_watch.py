918bde4a3483d12b02d233876e102dab834d5691
KierenJackson <kieren.jacksonclapper@my.jcu.edu.au>
2019-08-25 23:37:05 +1000

    Add menu selection function

--- movies_to_watch.py
+++ movies_to_watch.py
@@ -8,10 +8,20 @@ GitHub URL:
 def main():
     print("Movies To Watch 1.0 - by Kieren Jackson")
     print_menu()
+    selection = menu_selection()
 
 
 def print_menu():
     print("Menu: \nL - List movies \nA - Add new movie \nW - Watch a movie \nQ - Quit")
+
+
+def menu_selection():
+    answer = input(">>> ").upper()
+    while answer not in "LAWQ":
+        print("Invalid menu choice")
+        print_menu()
+        answer = input(">>> ").upper()
+    return answer
 
 
 if __name__ == '__main__':
